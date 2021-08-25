475c0b14e4ccf06fd8a1a3f9ca5249fac979d55c
Sefat Anam <37630292+sefatanam@users.noreply.github.com>
2021-08-25 16:14:21 +0600

    complete section 5

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,14 +1,37 @@
-// import react and react DOM
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
-// create react component
-const App = () => {
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, long: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (error) => console.log(error)
+      (position) => {
+        // to update state must call setState, never do direct assignment
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        });
+      },
+      (error) => {
+        this.setState({ errorMessage: error.message });
+      }
     );
-    return <div>Latitude : </div>;
-};
-// take the react component and show in the screen
-ReactDOM.render(<App/>, document.querySelector("#root"))
+  }
+
+  // must have to define
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error : {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>latitude : {this.state.lat}</div>;
+    }
+
+    return <div>Loading..</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
