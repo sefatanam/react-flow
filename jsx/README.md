ec242bb51a7474a0830d58478f555d8b6f029046
Sefat Anam <37630292+sefatanam@users.noreply.github.com>
2021-08-25 13:46:59 +0600

    components init

---
+++ jsx/README.md
@@ -0,0 +1,12 @@
+# Basics of this app
+
+## Look at 'src' directory 
+
+* Delete all default files <br>
+* Add a new file name "index.js" name is sensitive <br>
+* Run app then got empty page 
+
+
+## What ".jsx" is ?
+
+* 
---
+++ jsx/src/index.js
@@ -0,0 +1,9 @@
+// import react and react DOM
+import React from 'react';
+import ReactDOM from "react-dom";
+// create react component
+const App = () => {
+    return <div>Hi There</div>;
+};
+// take the react component and show in the screen
+ReactDOM.render(<App/> ,document.querySelector("#root"))
