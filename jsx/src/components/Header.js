bfb2003598797d301809f30b37f89fe2745a66af
sefatanam <sefatanam@gmail.com>
2021-09-15 00:35:14 +0600

    clients : streaming app init

---
+++ jsx/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const Header = () => {
+    return (
+        <div className="ui secondary pointing menu">
+            <Link to='/' className="item">Twitch</Link>
+            <div className="right menu">
+                <Link to='/' className='item'>Streams</Link>
+            </div>
+
+        </div>
+    );
+};
+
+export default Header;
