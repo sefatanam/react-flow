03b7370ae2e5b6d39ad4bb5c787d87fb634407d7
sefatanam <sefatanam@gmail.com>
2021-09-20 04:10:08 +0600

    feat : redux setup in client

--- jsx/src/components/GoogleAuth.js
+++ jsx/src/components/GoogleAuth.js
@@ -13,7 +13,7 @@ class GoogleAuth extends React.Component {
                 scope: 'email'
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                this.setState({isSignedIn: this.auth.isSignedIn.get()});
+                this.onAuthChange(this.auth.isSignedIn.get());
                 this.auth.isSignedIn.listen(this.onAuthChange);
             })
         })
@@ -21,7 +21,7 @@ class GoogleAuth extends React.Component {
 
     onAuthChange = isSignIn => {
         if (isSignIn) {
-            this.props.signIn();
+            this.props.signIn(this.auth.currentUser.get().getId());
         } else {
             this.props.signOut();
         }
@@ -30,9 +30,9 @@ class GoogleAuth extends React.Component {
 
 
     renderAuthButton() {
-        if (this.state.isSignedIn === null) {
+        if (this.props.isSignedIn === null) {
             return null;
-        } else if (this.state.isSignedIn) {
+        } else if (this.props.isSignedIn) {
             return (
 
                 <button className="ui red google button" onClick={() => this.auth.signOut()}>
